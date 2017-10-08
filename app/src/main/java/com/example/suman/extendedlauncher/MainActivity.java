58d1850e33e21914fd2e3bab740ccba35475b535
suman neupane <sumanmyon.sam@gmail.com>
2017-10-08 09:23:24 +0545

    first 2017108

---
+++ app/src/main/java/com/example/suman/extendedlauncher/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.suman.extendedlauncher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//
+//
+//…or create a new repository on the command line
+//
+//        echo "# ExtendedLauncher" >> README.md
+//        git init
+//        git add README.md
+//        git commit -m "first commit"
+//        git remote add origin https://github.com/sumanmyon/ExtendedLauncher.git
+//        git push -u origin master
+//        …or push an existing repository from the command line
+//
+//        git remote add origin https://github.com/sumanmyon/ExtendedLauncher.git
+//        git push -u origin master
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
